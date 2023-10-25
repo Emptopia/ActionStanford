5cab43811479eb6f09ab4534ac9836a6546d7679
Emptopia <415671502@qq.com>
2023-10-25 15:33:55 +0800

    CommitTest

---
+++ Source/ActionStanford/ActionStanfordGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionStanfordGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONSTANFORD_API AActionStanfordGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
